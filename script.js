103f5bfeded365c59b0c95960c4faf62c7c83aa4
jhonathancs <jhonathan_jcs@hotmail.com>
2021-07-20 11:26:36 -0500

    Commit
    projeto

---
+++ script.js
@@ -0,0 +1,13 @@
+const loginInput = document.getElementById('login');
+const senhaInput = document.getElementById('senha');
+const entrarBTN = document.getElementById('entrar');
+
+function clicar() {
+   if (loginInput.value == 'tryber@teste.com' && senhaInput.value == 123456) {
+      alert('Olá, Tryber!')
+   } else {
+      alert('Login ou senha inválidos.')
+}
+};
+
+// entrarBTN.addEventListener('click',clicar);
